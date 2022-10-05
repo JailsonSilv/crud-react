5bb6ec12b0d5e968cb1970d65ed8aea1cf926a6c
Jailson Silva <104876290+JailsonSilv@users.noreply.github.com>
2022-10-05 16:03:05 -0300

    configuração do db pasta server

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const app = express();
+//Começando a conexao de dados mysql
+const mysql = require("mysql");
+const cors = require("cors"); 
 
-// app.get  === Fazer requisições pegar valores
-// app.post === Enviar fazer solicitações
-// app.delete === deletar
-// app.put === editar arquivos 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "crudgame",
+});
 
-app.get('/', (req, res) => {
-    res.send("Vai da muito certo")
+// verificando se esta ok e execultando cd mysql
+app.get("/", (req, res) => {
+
+    let SQL = 
+        "INSERT INTO gametable ( name, cost, category ) VALUES ('FIFA 23', '190', 'Futebol')";
+
+    db.query(SQL, (err, result) => {
+        console.log(err);
+    });
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+   const { name } = req.body;
+   const { cost } = req.body;
+   const { category } = req.body;
+
+   console.log(name);
 })
 
-app.listen(3001, ()=>{
+app.listen(3001, () => {
     console.log("Rodando servidor!!!")
-})
+});
